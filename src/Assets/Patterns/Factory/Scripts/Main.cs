5de3f88faec55430fa1a0372b550c80a9a69473e
qdiaps <qdiaps@gmail.com>
2024-09-17 19:58:50 +0300

    Add pattern Factory

---
+++ src/Assets/Patterns/Factory/Scripts/Main.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Patterns.Factory.Scripts
+{
+    public class Main : MonoBehaviour
+    {
+        private void Start()
+        {
+            var factory = new PlayerFactory();
+            var player = factory.Create("Player", new Vector3(10, 5, 30), Quaternion.identity);
+            Debug.Log($"Игрок {player.name} создан");
+        }
+    }
+}
---
+++ src/Assets/Patterns/Factory/Scripts/PlayerFactory.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Patterns.Factory.Scripts
+{
+    public class PlayerFactory
+    {
+        public GameObject Create(string name, Vector3 position, Quaternion rotation)
+        {
+            var player = new GameObject(name);
+            player.transform.position = position;
+            player.transform.rotation = rotation;
+            return player;
+        }
+    }
+}
